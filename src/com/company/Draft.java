40d5f1d2ef8322b325a63ea7b03f7eb5c9832ab5
BosykAnton <Casper89@336937>
2020-04-16 20:11:16 +0300

    Начал JavaRush

---
+++ src/com/company/Draft.java
@@ -0,0 +1,19 @@
+package com.company;
+/*
+Реализуем метод print
+*/
+public class Draft {
+
+    public static void main(String[] args) {
+        print("Java is easy to learn!");
+        print("Java is object-oriented!");
+
+
+    }
+    public static void print(String s) {
+        System.out.println(s);
+        System.out.println(s);
+
+    }
+}
+
---
+++ src/com/javarush/task/task01/task0108/Solution.java
@@ -0,0 +1,24 @@
+package com.javarush.task.task01.task0108;
+
+/*Закомментируй ненужные строки кода, чтобы на экран вывелась надпись:
+2 плюс 3 равно 5
+
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        int a = 3;
+        int b = 2;
+
+        //System.out.print("два");
+        System.out.print(b);
+        System.out.print(" плюс ");
+        //System.out.print(" минус ");
+        System.out.print(a);
+        //System.out.print("три");
+        System.out.print(" равно ");
+        //System.out.print(" будет ");
+        //System.out.print("пять");
+        System.out.print(a + b);
+    }
+}
